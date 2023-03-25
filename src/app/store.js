38f0f49600d15e449bc1c1f371daa93b0a9211cc
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-03-25 11:08:09 +0600

    Complite UI

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import apiSlice from "../features/api/apiSlice";
 
 export default store = configureStore({
-  reducer: {},
-  middleware: (defaultMiddleware) => defaultMiddleware(),
+  reducer: { [apiSlice.reducerPath]: apiSlice.reducer },
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware().concat(apiSlice.middleware),
   devTools: !import.meta.env.MODE === "production",
 });
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,5 +1,7 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const apiSlice = createApi({
+export default apiSlice = createApi({
   reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_TITLE }),
+  endpoints: (builder) => ({}),
 });
