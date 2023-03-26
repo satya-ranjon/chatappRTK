a8e6a4add084cb91b27e39582908061faa1aeb06
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-03-26 12:14:42 +0600

    Add conversation and messages Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import apiSlice from "../features/api/apiSlice";
 import authReducer from "../features/auth/authSlice";
+import conversationReducer from "../features/conversations/conversationSlice";
+import messagesReducer from "../features/messages/messagesSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    conversations: conversationReducer,
+    messages: messagesReducer,
   },
+
   middleware: (defaultMiddleware) =>
     defaultMiddleware().concat(apiSlice.middleware),
+
   devTools: !(import.meta.env.MODE === "production"),
 });
+
 export default store;
